00192d521dead1c67c33f0aecc12e03b28757c17
Nicolas-zero <nicolaschedraui@gmail.com>
2025-04-10 09:02:19 -0500

    feat: :fire: avances

---
+++ Dia1/menu.js
@@ -0,0 +1,37 @@
+alert("Bienvenido a Campuslands")
+const option= prompt(
+"¿Con quien qieres iniciar sesion?\n"+
+"1. Camper\n"+ 
+"2. Trainer\n"+ 
+"3. Coordinador ")
+
+switch(option) {
+    case "1":
+        alert("Bienvenido Camper")
+         option= prompt(
+            "¿que deseas hacer?\n"+
+            "1. Ver informacion\n"+
+            "2. Ver ruta de entrenamiento\n"
+         )
+
+        
+}
+
+switch(option) {
+    case "2":
+        alert("Bienvenio Trainer")
+        option=prompt(
+            "¿que deseas hacer?\n"+
+            "1. "
+        )
+
+}
+
+switch(option) {
+    case "3":
+        alert("Bienveido Coordinador")
+        option=prompt(
+            "¿que deseas hacer?\n"+
+            "1. "
+        )
+}
