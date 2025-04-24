52c6b9c86d56ae9fb6c9ea39469e6dadd6a55d62
Nicolas-zero <nicolaschedraui@gmail.com>
2025-04-24 08:27:38 -0500

    feat: :construction: blackjack api game

---
+++ Dia6/script/card.js
@@ -0,0 +1,8 @@
+function ocultarMenu() {
+    var menu = document.getElementById('menu-inicio');
+    menu.style.display = 'none'; // Oculta el menú
+    // o
+    menu.style.visibility = 'hidden'; // Oculta el menú y deja espacio en el layout
+}
+
+<button onclick="jugar()">JUGAR</button>
